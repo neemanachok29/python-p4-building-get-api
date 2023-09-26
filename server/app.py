2ad262bf24035f1fead30629f70fe64351c71b2e
Neema Nachok <neemanachok29@gmail.com>
2023-09-26 18:16:36 +0300

    done

--- server/app.py
+++ server/app.py
@@ -3,19 +3,29 @@ from flask import Flask, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from models import db, User, Review, Game
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
+
 migrate = Migrate(app, db)
+
 db.init_app(app)
+
 @app.route('/')
+
+@app.route("/")
 def index():
     return "Index for Game/Review/User API"
 
-@app.route('/games')
-def games():
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
 
+@app.route("/games")
+def games():
     games = []
     for game in Game.query.all():
         game_dict = {
@@ -26,29 +36,30 @@ def games():
         }
         games.append(game_dict)
 
-    response = make_response(
-        jsonify(games),
-        200
-    )
-
-    return response
-
-@app.route('/games/<int:id>')
-def game_by_id(id):
-    game = Game.query.filter(Game.id == id).first()
-
-    game_dict = game.to_dict()
-
-    response = make_response(
-        # it still needs to be JSON, after all
-        jsonify(game_dict),
-        200
-    )
+    response = make_response(jsonify(games), 200)
     response.headers["Content-Type"] = "application/json"
 
     return response
 
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
-    app.run(port=5555, debug=True)
+@app.route("/games/<int:id>")
+def game_by_id(id):
+    game = Game.query.filter_by(id=id).first()
+
+    game_dict = game.to_dict()
+
+    # game_dict = {
+    #     "title": game.title,
+    #     "genre": game.genre,
+    #     "platform": game.platform,
+    #     "price": game.price,
+    # }
+
+    response = make_response(jsonify(game_dict), 200)
+    response.headers["Content-Type"] = "application/json"
+
+    return response
+
+
+if __name__ == "__main__":
+    app.run(port=5555)
